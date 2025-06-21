8844e1f0f7bd648683a4a376c6d5526b08ade968
Jyoti Gupta <gupta.jyoti0811@gmail.com>
2025-06-21 12:48:46 +0530

    Initial commit

---
+++ swap.py
@@ -0,0 +1,20 @@
+#swap two numbers
+#method 1
+x=30
+y=40
+print("value of x is :",x)
+print("value of y is :",y)
+temp=x
+x=y
+y=temp
+print("new value of x is :",x)
+print("new value of y is :",y)
+
+#method 2
+x=90
+y=30
+print("value of x is :",x)
+print("value of y is :",y)
+x,y=y,x
+print("new value of x is :",x)
+print("new value of y is :",y)
